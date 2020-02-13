557e0d447af0783f7120936f09881b42cf91b494
jeremyyuan0322 <jeremy.yuan0322@gmail.com>
2020-02-14 03:11:35 +0800

    add

---
+++ test/Assets/script/enter name.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class wenzi : MonoBehaviour
+{
+
+	//主摄像机对象
+	private Camera camera;
+	//NPC名称
+	public string name;
+
+	//主角对象
+	GameObject hero;
+	//NPC模型高度
+	float npcHeight;
+
+	//默认NPC血值
+	private int HP = 100;
+
+	void Start()
+	{
+		//得到主角对象
+		hero = this.gameObject;
+		//得到摄像机对象
+		camera = Camera.main;
+
+	
+	}
+
+	void OnGUI()
+	{
+		//得到NPC头顶在3D世界中的坐标
+		//默认NPC坐标点在脚底下，所以这里加上npcHeight它模型的高度即可
+		Vector3 worldPosition = new Vector3(transform.position.x, transform.position.y + npcHeight, transform.position.z);
+		//根据NPC头顶的3D坐标换算成它在2D屏幕中的坐标
+		Vector2 position = camera.WorldToScreenPoint(worldPosition);
+		//得到真实NPC头顶的2D坐标
+		position = new Vector2(position.x, Screen.height - position.y);
+
+		//计算NPC名称的宽高
+		Vector2 nameSize = GUI.skin.label.CalcSize(new GUIContent(this.name));
+		//根据tag设置显示颜色
+		if (this.tag == "green")
+			GUI.color = Color.green;
+		else if (this.tag == "red")
+			GUI.color = Color.red;
+		GUI.skin.label.fontSize = 18;//字体大小
+									 //绘制NPC名称
+		GUI.Label(new Rect(position.x - (nameSize.x / 2), position.y - nameSize.y, nameSize.x, nameSize.y), name);
+
+
+
+	}
+}
---
+++ test/Assets/script/playermove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position -= new Vector3(speed, 0, 0);
+        }
+
+    }
+}
