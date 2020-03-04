f9a4352e63f585cf98b89bc3c5adcbe23cd5e823
jeremyyuan0322 <jeremy.yuan0322@gmail.com>
2020-03-05 01:16:58 +0800

    mine

---
+++ newgame/Assets/scipt/Nameinput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Nameinput : MonoBehaviour
+{
+    public string playername;
+    public string entertext;
+
+    
+ 
+    void Start()
+    {
+     
+
+    }
+
+  
+    void Update()
+    {
+        
+    }
+    public void Name_Enter()
+    {
+        playername = entertext;
+        print((string)playername);
+    }
+    
+    public void Gamestart()
+    {
+        SceneManager.LoadScene("game");
+    }
+    public void Name_print()
+    {
+        
+        print(playername);
+    }
+}
